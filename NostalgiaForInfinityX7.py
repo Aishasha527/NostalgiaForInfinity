56ff2fe56d8b7b2ef36bf3abfbedc8b1372f04fb
iterativ <vangelis.dimou@tutanota.com>
2026-02-24 21:27:19 +0200

    X7: signal 4: add protection.

--- NostalgiaForInfinityX7.py
+++ NostalgiaForInfinityX7.py
@@ -13682,6 +13682,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["RSI_3_1h"] > 50.0) | (df["STOCHRSIk_14_14_3_3_4h"] < 90.0) | (df["ROC_9_4h"] < 40.0))
             # 1h down move, 1h high & overbought
             & ((df["RSI_3_1h"] > 55.0) | (df["STOCHRSIk_14_14_3_3_1h"] < 80.0) | (df["ROC_9_1h"] < 10.0))
+            # 1h down move, 4h & 1d overbought
+            & ((df["RSI_3_1h"] > 60.0) | (df["ROC_9_4h"] < 30.0) | (df["ROC_9_1d"] < 60.0))
             # 4h down move, 4h high, 4h downtrend
             & ((df["RSI_3_4h"] > 3.0) | (df["AROONU_14_4h"] < 50.0) | (df["ROC_9_4h"] > -40.0))
             # 4h & 1d down move, 1h still not low enough
@@ -13724,6 +13726,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["AROONU_14_4h"] < 30.0) | (df["ROC_9_4h"] > -40.0) | (df["ROC_9_1d"] > -40.0))
             # 4h still high, 5m downtrend
             & ((df["AROONU_14_4h"] < 40.0) | (df["ROC_9"] > -40.0))
+            # 4h high, 4h & 1d overbought
+            & ((df["AROONU_14_4h"] < 60.0) | (df["ROC_9_4h"] < 30.0) | (df["ROC_9_1d"] < 60.0))
             # 4h & 1d high, 4h overbought
             & ((df["AROONU_14_4h"] < 100.0) | (df["AROONU_14_1d"] < 100.0) | (df["ROC_9_4h"] < 100.0))
             # 4h high, 1h & 1d overbought
@@ -13734,6 +13738,8 @@ class NostalgiaForInfinityX7(IStrategy):
             & ((df["AROONU_14_1d"] < 70.0) | (df["ROC_9_4h"] > -40.0) | (df["ROC_9_1d"] > -40.0))
             # 1d high, 1h & 1d overbought
             & ((df["AROONU_14_1d"] < 80.0) | (df["ROC_9_1h"] < 30.0) | (df["ROC_9_1d"] < 200.0))
+            # 1d high, 4h & 1d overbought
+            & ((df["AROONU_14_1d"] < 90.0) | (df["ROC_9_4h"] < 30.0) | (df["ROC_9_1d"] < 60.0))
             # 4h & 1d overbought
             & ((df["ROC_9_4h"] < 100.0) | (df["ROC_9_1d"] < 200.0))
             # drop in last 20 days, 1h high, 1d downtrend
